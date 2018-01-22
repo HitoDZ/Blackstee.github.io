063b94f277846191d27cc9d842de364bdda95d28
Lev Dzyunyak <dzyunyakl@gmail.com>
2018-01-22 13:27:03 +0200

    Changed the entire back-end. Added routing, database and model binding.
    Open project in Visual Studio, then open package manager console and use command "update-database". Insert your data into database after. Stable DB coming soon.

---
+++ MemsMVC/MemesContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MemsMVC
+{
+    public class MemesContext : DbContext
+    {
+        public MemesContext(DbContextOptions<MemesContext> options) : base(options) { }
+
+        public DbSet<Models.Meme> Memes { get; set; }
+    }
+}
